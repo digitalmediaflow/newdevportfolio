b68783a2568c600af753ff70774728af0ea84dec
“Robert <digitalmediaflow@gmail.com>
2019-07-29 03:59:44 -0400

    added bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+   title = models.CharField(max_length=255)
+   date = models.DateTimeField(auto_now_add = True)
+   description = models.TextField()
+   image = models.ImageField(upload_to="images/")
